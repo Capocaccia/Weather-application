0bd82ac68bdc9b2734dec815c14a85e6bdcef919
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-09-05 18:25:21 -0500

    cleaned up code and added functions for particular trails.

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,29 @@
 var API_URL = 'http://api.wunderground.com/api/7a47b141d9b58611/conditions/q/'
 var lookup = document.querySelector('.lookup');
+var geo = document.querySelector('.geoLocate');
+var pwmtb = document.querySelector('.pwmtb');
+var hct = document.querySelector('.hct');
+var stanky = document.querySelector('.stanky');
+var gMtn = document.querySelector('.gMtn');
 
 lookup.onclick = function() {
   var input = document.querySelector('input');
   var zip = input.value;
-
   getJSON(API_URL + zip + '.json', function (data) {
       var hum = document.querySelector('.hum');
       var city = document.querySelector('.city');
       var sky = document.querySelector('.sky');
       var win = document.querySelector('.win');
-      var hum = document.querySelector('.hum');
       var tempf = document.querySelector('.tempf');
+      var precip = document.querySelector('.precip');
 
       hum.innerHTML = data.current_observation.relative_humidity;
       city.innerHTML = data.current_observation.observation_location.city;
       sky.innerHTML = data.current_observation.weather;
       win.innerHTML = data.current_observation.wind_mph;
       tempf.innerHTML = data.current_observation.temp_f;
-      console.log("searching")
-      console.log(data.current_observation.temp_f)
-      if(data.current_observation.temp_f > 50 ){
-
-      }
+      precip.innerHTML = data.current_observation.precip_today_in;
+      console.log(data)
   });
 }
 
@@ -39,44 +40,12 @@ function getJSON(url, cb) {
   xhr.send();
 }
 
-//SPLIT BETWEEN THE TWO BUTTON ACTIONS.
-//Button functions above.
-//Table containing 7 day forecast / geolocation below
-
-var API_URL = 'http://api.wunderground.com/api/7a47b141d9b58611/conditions/q/'
-var loc = document.querySelector('.location');
-
-loc.onclick = function() {
+geo.onclick = function() {
   navigator.geolocation.getCurrentPosition(function(position) {
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
 
     getJSON(API_URL + lat + "," + long + '.json', function (data) {
-      var hum = document.querySelector('.hum');
-      var city = document.querySelector('.city');
-      var sky = document.querySelector('.sky');
-      var win = document.querySelector('.win');
-      var humi = document.querySelector('.hum');
-      var tempf = document.querySelector('.tempf');
-
-      hum.innerHTML = data.current_observation.relative_humidity;
-      city.innerHTML = data.current_observation.observation_location.city;
-      sky.innerHTML = data.current_observation.weather;
-      win.innerHTML = data.current_observation.wind_mph;
-      humi.innerHTML = data.current_observation.relative_humidity;
-      tempf.innerHTML = data.current_observation.temp_f;
-      console.log(data)
-    });
-  });
-};
-
-window.onload = function() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-
-    getJSON(API_URL + lat + "," + long + '.json', function (data) {
-      var span = document.querySelector('span');
       var hum = document.querySelector('.hum');
       var city = document.querySelector('.city');
       var sky = document.querySelector('.sky');
@@ -84,13 +53,93 @@ window.onload = function() {
       var tempf = document.querySelector('.tempf');
       var precip = document.querySelector('.precip');
 
-      span.innerHTML = data.current_observation.temp_f;
+      city.innerHTML = data.current_observation.observation_location.city;
+      sky.innerHTML = data.current_observation.weather;
+      win.innerHTML = data.current_observation.wind_mph;
+      hum.innerHTML = data.current_observation.relative_humidity;
+      precip.innerHTML = data.current_observation.precip_today_in;
+      tempf.innerHTML = data.current_observation.temp_f;
+    });
+  });
+};
+
+pwmtb.onclick = function() {
+  var zip = 37205
+
+  getJSON(API_URL + zip + '.json', function (data) {
+      var hum = document.querySelector('.hum');
+      var city = document.querySelector('.city');
+      var sky = document.querySelector('.sky');
+      var win = document.querySelector('.win');
+      var tempf = document.querySelector('.tempf');
+      var precip = document.querySelector('.precip');
+
       hum.innerHTML = data.current_observation.relative_humidity;
       city.innerHTML = data.current_observation.observation_location.city;
       sky.innerHTML = data.current_observation.weather;
       win.innerHTML = data.current_observation.wind_mph;
-      tempf.innerHTML = data.current_observation.temp_f;
       precip.innerHTML = data.current_observation.precip_today_in;
-    });
+      tempf.innerHTML = data.current_observation.temp_f;
   });
-};
+}
+
+hct.onclick = function() {
+  var zip = 37217
+
+  getJSON(API_URL + zip + '.json', function (data) {
+      var hum = document.querySelector('.hum');
+      var city = document.querySelector('.city');
+      var sky = document.querySelector('.sky');
+      var win = document.querySelector('.win');
+      var tempf = document.querySelector('.tempf');
+      var precip = document.querySelector('.precip');
+
+      hum.innerHTML = data.current_observation.relative_humidity;
+      city.innerHTML = data.current_observation.observation_location.city;
+      sky.innerHTML = data.current_observation.weather;
+      win.innerHTML = data.current_observation.wind_mph;
+      precip.innerHTML = data.current_observation.precip_today_in;
+      tempf.innerHTML = data.current_observation.temp_f;
+  });
+}
+
+
+stanky.onclick = function() {
+  var zip = 38134
+
+  getJSON(API_URL + zip + '.json', function (data) {
+      var hum = document.querySelector('.hum');
+      var city = document.querySelector('.city');
+      var sky = document.querySelector('.sky');
+      var win = document.querySelector('.win');
+      var tempf = document.querySelector('.tempf');
+      var precip = document.querySelector('.precip');
+
+      hum.innerHTML = data.current_observation.relative_humidity;
+      city.innerHTML = data.current_observation.observation_location.city;
+      sky.innerHTML = data.current_observation.weather;
+      win.innerHTML = data.current_observation.wind_mph;
+      precip.innerHTML = data.current_observation.precip_today_in;
+      tempf.innerHTML = data.current_observation.temp_f;
+  });
+}
+
+gMtn.onclick = function() {
+  var zip = 80228
+
+  getJSON(API_URL + zip + '.json', function (data) {
+      var hum = document.querySelector('.hum');
+      var city = document.querySelector('.city');
+      var sky = document.querySelector('.sky');
+      var win = document.querySelector('.win');
+      var tempf = document.querySelector('.tempf');
+      var precip = document.querySelector('.precip');
+
+      hum.innerHTML = data.current_observation.relative_humidity;
+      city.innerHTML = data.current_observation.observation_location.city;
+      sky.innerHTML = data.current_observation.weather;
+      win.innerHTML = data.current_observation.wind_mph;
+      precip.innerHTML = data.current_observation.precip_today_in;
+      tempf.innerHTML = data.current_observation.temp_f;
+  });
+}
